96cb379a79294b48e39c3f39adf1fe8ec38d0a4e
Elliot Kumedzro <ebkumedzro@st.ug.edu.gh>
2024-05-07 15:30:29 +0100

    Footer UI

--- src/Missing.js
+++ src/Missing.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Missing = () => {
   return (
-    <main>
-        <h1>Missing</h1>
+    <main className='Missing'>
+        <h1>Page Not Found</h1>
+        <p>
+          <Link to={"/"}>
+              Vist our HomePage
+          </Link> 
+        </p>
     </main>
   )
 }
